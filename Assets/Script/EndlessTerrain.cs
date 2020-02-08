40a78bfb09dd3c2c0571155374fbf358ea1565fb
Nghviet <44728972+Nghviet@users.noreply.github.com>
2020-02-08 22:13:35 +0700

    Initial commit

---
+++ Assets/Script/EndlessTerrain.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+
+    const float viewerMoveThresholdForChunkUpdate = 8f;
+    const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
+
+    public LODInfo[] detailLevels;
+    public static float maxViewDst;
+
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    Vector2 viewerPositionOld;
+    static MapGenerator mapGenerator;
+    int chunkSize;
+    int chunksVisible;
+    public Material material;
+
+    Dictionary<Vector2, TerrainChunk> terrainDict = new Dictionary<Vector2, TerrainChunk>();
+    static List<TerrainChunk> visibleLastUpdate = new List<TerrainChunk>();
+
+    private void Start()
+    {
+        maxViewDst = detailLevels[detailLevels.Length - 1].visibleDistThreshold;
+
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisible = Mathf.RoundToInt(maxViewDst / chunkSize);
+        mapGenerator = FindObjectOfType<MapGenerator>();
+
+        UpdateVisibleChunks();
+    }
+
+    private void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        if((viewerPositionOld-viewerPosition).sqrMagnitude>sqrViewerMoveThresholdForChunkUpdate)
+        {
+            UpdateVisibleChunks();
+            viewerPositionOld = viewerPosition;
+        }
+        
+    }
+    
+    void UpdateVisibleChunks()
+    {
+        for(int i = 0; i < visibleLastUpdate.Count; i++)
+        {
+            visibleLastUpdate[i].SetVisible(false);
+        }
+
+        int currentChunkX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for(int yOffset = -chunksVisible; yOffset <= chunksVisible; yOffset++)
+        {
+            for (int xOffset = -chunksVisible; xOffset <= chunksVisible; xOffset++)
+            {
+                Vector2 viewed = new Vector2(currentChunkX + xOffset, currentChunkY + yOffset);
+                if(terrainDict.ContainsKey(viewed))
+                {
+                    terrainDict[viewed].UpdateChunk();
+                }
+                else
+                {
+                    terrainDict.Add(viewed, new TerrainChunk(viewed,chunkSize, detailLevels, transform,material));
+                }
+            }
+        }
+    }
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        MeshRenderer meshRenderer;
+        MeshFilter meshFilter;
+
+        LODInfo[] detailLevels;
+        LODMesh[] lodMeshes;
+
+        MapData mapData;
+        bool mapDataReveiced;
+        int previousLODIndex = -1;
+
+        public TerrainChunk(Vector2 coord,int size, LODInfo[] detailLevels,Transform parent, Material material)
+        {
+            this.detailLevels = detailLevels;
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+            print(bounds);
+            //meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject = new GameObject("Terrain Chunk");
+            meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshFilter = meshObject.AddComponent<MeshFilter>();
+
+            meshRenderer.material = material;
+            meshObject.transform.position = positionV3;
+            meshObject.transform.parent = parent;
+
+            SetVisible(false);
+
+            lodMeshes = new LODMesh[detailLevels.Length];
+            for(int i = 0; i < detailLevels.Length; i++)
+            {
+                lodMeshes[i] = new LODMesh(detailLevels[i].lod,UpdateChunk);
+            }
+             
+            mapGenerator.RequestMapData(position,OnMapDataReceived);
+        }
+
+
+        void OnMapDataReceived(MapData mapData)
+        {
+            this.mapData = mapData;
+            mapDataReveiced = true;
+
+            Texture2D texture = TextureGenerator.TextureFromColorMap(mapData.colorMap, MapGenerator.mapChunkSize, MapGenerator.mapChunkSize);
+            meshRenderer.material.mainTexture = texture;
+
+            UpdateChunk();
+        }
+
+        void OnMeshDataReceived(MeshData meshData)
+        {
+            meshFilter.mesh = meshData.CreateMesh();
+        }
+
+        public void UpdateChunk()
+        {
+            if (mapDataReveiced)
+            {
+                float distance = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+                bool visible = distance <= maxViewDst;
+
+                if (visible)
+                {
+                    int lodIndex = 0;
+                    for (int i = 0; i < detailLevels.Length - 1; i++)
+                    {
+                        if (distance > detailLevels[i].visibleDistThreshold)
+                        {
+                            lodIndex = i + 1;
+                        }
+                        else break;
+                    }
+
+                    if (lodIndex != previousLODIndex)
+                    {
+                        LODMesh lodMesh = lodMeshes[lodIndex];
+                        if (lodMesh.hasMesh)
+                        {
+                            previousLODIndex = lodIndex;
+                            meshFilter.mesh = lodMesh.mesh;
+                        }
+                        else if (!lodMesh.hasRequestedMesh)
+                        {
+                            lodMesh.RequestMesh(mapData);
+                        }
+                    }
+
+                    visibleLastUpdate.Add(this);
+                }
+
+                SetVisible(visible);
+            }  
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool isVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+
+    class LODMesh
+    {
+        public Mesh mesh;
+        public bool hasRequestedMesh;
+        public bool hasMesh;
+        int lod;
+        System.Action updateCallback;
+
+        public LODMesh(int lod, System.Action updateCallback)
+        {
+            this.lod = lod;
+            this.updateCallback = updateCallback;
+        }
+
+        void OnMeshDataReceived(MeshData meshData)
+        {
+            mesh = meshData.CreateMesh();
+            hasMesh = true;
+
+            updateCallback();
+        }
+
+        public void RequestMesh(MapData mapData)
+        {
+            hasRequestedMesh = true;
+            mapGenerator.RequestMeshData(mapData, lod, OnMeshDataReceived);
+        }
+    }
+
+    [System.Serializable]
+    public struct LODInfo
+    {
+        public int lod;
+        public float visibleDistThreshold;
+    }
+
+}
---
+++ Assets/Script/Noise.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
+    {
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        System.Random rand = new System.Random(seed);
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = rand.Next(-100000, 100000) * 1 + offset.x;
+            float offsetY = rand.Next(-100000, 100000) * 1 - offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x - halfWidth + octaveOffsets[i].x) / scale * frequency;
+                    float sampleY = (y - halfHeight + octaveOffsets[i].y) / scale * frequency;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight) maxNoiseHeight = noiseHeight;
+                if (noiseHeight < minNoiseHeight) minNoiseHeight = noiseHeight;
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            } 
+        }
+
+        return noiseMap;
+    }
+
+}
